269788db7e233ebc6c1f3ed57534aa93d8721f7f
= <=>
2024-02-22 10:16:26 -0500

    AGREGAR EL APARTADO DE CONÓCEME DONDE RESUMO BREVEMENTE MIS HABILIDADES Y CONOCIMIENTOS

--- js/function.js
+++ js/function.js
@@ -1,7 +1,36 @@
+// USAR MODO ESTRICTO
+
 "use strict"
+
+// Crear constantes y variables
 
 const firstname = document.getElementById('firstname')
 const secondname = document.getElementById('secondname')
+const iconMore = document.querySelector('.conoceme__arrow i')
+const conMore = document.querySelector('.conoceme__option')
+let mediaQuery = window.matchMedia('(max-width: 600px)')
+
+const toggleMore = () => {
+    if (conMore.style.display === 'none' || !conMore.style.display) {
+        conMore.style.display = 'flex';
+        iconMore.style.transform = 'rotate(180deg)';
+        iconMore.style.transition = 'transform 500ms ease';
+    } else {
+        conMore.style.display = 'none';
+        iconMore.style.transform = 'rotate(0)';
+        iconMore.style.transition = 'transform 500ms ease';
+    }
+};
+
+iconMore.addEventListener('click', toggleMore);
+
+if (mediaQuery.matches) {
+    iconMore.addEventListener('click', toggleMore);
+} else {
+    iconMore.addEventListener('mouseover', toggleMore);
+    conMore.addEventListener('mouseleave', toggleMore);
+}
+
 
 const hoverNames = (element, nuevo, ant) =>{
     element.textContent = nuevo

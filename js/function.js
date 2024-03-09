595f16e1167f1a122fac362b5160cc0476060cbb
= <=>
2024-03-09 14:59:06 -0500

    Agregar lenguajes que domino

--- js/function.js
+++ js/function.js
@@ -40,3 +40,42 @@ const hoverNames = (element, nuevo, ant) =>{
 firstname.addEventListener('mouseover', ()=>hoverNames(firstname, 'Conocimientos', 'Jesús'))
 
 secondname.addEventListener('mouseover', ()=>hoverNames(secondname, 'Experimentos', 'Sánchez'))
+
+// APARTADO CONOCIMIETOS
+
+document.getElementById('language-html').textContent = '<HTML />';
+
+const wordsSQL = ['SELECT', 'SQL', 'INTO TO', 'UPDATE', 'AS', 'DELETE', 'UNION']
+
+const numAleatorio = arr => {
+    let num = Math.floor(Math.random() * arr.length);
+    return num
+}
+
+document.getElementById('language-sql').addEventListener('click', ()=> {
+    let numGen = numAleatorio(wordsSQL)
+    document.getElementById('language-sql').textContent = wordsSQL[numGen]
+})
+
+const wordGit = document.getElementById('language-git')
+
+const animationsGit = (nameanimation, text) => {
+    wordGit.style.animationName = nameanimation
+    wordGit.style.animationDuration = '1s'
+    wordGit.style.animationDirection = 'alternate'
+    wordGit.style.animationTimingFunction = 'ease-in-out'
+    wordGit.style.animationIterationCount = 'infinite'
+    wordGit.textContent = text
+}
+
+wordGit.addEventListener('click', ()=>{
+    let versions = [
+        ['rebotar', 'Versión 1'],
+        ['aparecer', 'Versión 2'],
+        ['desplazar', 'Versión 3'],
+        ['arcoiris', 'Versión 4'],
+        ['null', 'GIT']
+    ]
+    let numAl = numAleatorio(versions)
+    animationsGit(versions[numAl][0], versions[numAl][1])
+})
